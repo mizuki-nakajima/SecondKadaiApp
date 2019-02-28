cf1f059af55b90ccdff54e85e079501b54f1d483
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-02-28 19:10:02 +0900

    ver1

---
+++ ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Nakajima Mizuki on 2019/02/26.
+//  Copyright © 2019 Nakajima Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // 2画面目のLabelをViewControllerにIBOutletとして接続しておく
+    @IBOutlet weak var label: UILabel!
+    
+    // 入力した名前を受け取るためのプロパティ（変数）を宣言しておく
+    var nameView : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    // 1画面目のViewControllerから遷移するときにprepareForSegueで
+    //nameViewには入力された名前が入っている
+         label.text = "結果は \(nameView) です"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate {
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    // 文字列保存用の変数
+    var nameset: String!
+    
+    @IBOutlet weak var inputed_character: UITextField!
+    // TextFieldから文字を取得してnamesetに代入する
 
+    
+    //「次へ」を押したときの動き
+    @IBAction func handle(_ sender: Any) {
+        //「次へ」が押されたら遷移先のLabelの表示をnamesetの値に変える
 
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているnameViewにnameの値を代入して渡す
+        resultViewController.nameView = inputed_character.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+     // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 }
 
